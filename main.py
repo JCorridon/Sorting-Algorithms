63659f2cb3e685fcafdba193351517361112708f
JCorridon <JohnCorridon23@gmail.com>
2024-06-12 13:40:34 -0400

    added files

---
+++ main.py
@@ -0,0 +1,6 @@
+import createList
+import insertionSort
+
+len_list = int(input("How long would you like for the list to be?: "))
+list_type = int(input("Enter 0 for sorted list, 1 for random list, or 2 for reverse sorted list: "))
+
