d34e42d96a339d7bfeb4f426b7e51ed236152516
JCorridon <JohnCorridon23@gmail.com>
2024-06-12 20:05:22 -0400

    added selection sort

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import createList
 import insertionSort
 
-len_list = int(input("How long would you like for the list to be?: "))
-mag_list = int(input("how large would you like the numbers to be?: "))
-list_type = int(input("Enter 0 for sorted list, 1 for random list, or 2 for reverse sorted list: "))
+print('seperated by a space input the Length of List, magnitude of list, 0 for sorted 1 for random or 2 for reverse sorted list, number of runs')
+string_List = input().split()
+print(string_List)
+
+len_list = int(string_List[0])
+mag_list = int(string_List[1])
+list_type = int(string_List[2])
+num_runs = int(string_List[3])
 
 if list_type == 0:
-    print(createList.sortedList(len_list, mag_list))
+    for i in range(num_runs):
+        A = createList.sortedList(len_list, mag_list)
+        insertionSort.insertionSort(A)
 
 elif list_type == 1:
-    print(createList.randomList(len_list, mag_list))
+    for i in range(num_runs):
+        A = createList.randomList(len_list, mag_list)
+        insertionSort.insertionSort(A)
 
 elif list_type == 2:
-    print(createList.reverseList(len_list, mag_list))
+    for i in range(num_runs):
+        A = createList.reverseList(len_list, mag_list)
+        insertionSort.insertionSort(A)
 
 else:
     print('please enter valid choice for list type')
