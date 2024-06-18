130fb6fdd9d5cbfb0eea3118dcc265c2a7744ffb
JCorridon <JohnCorridon23@gmail.com>
2024-06-18 13:00:37 -0400

    Added new search techniques

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import createList
-import insertionSort
+import sortAlgorithms
+import time
 
-print('seperated by a space input the Length of List, magnitude of list, 0 for sorted 1 for random or 2 for reverse sorted list, number of runs')
+print('''seperated by a space input 
+      the Length of List, 
+      magnitude of list, 
+      0 for sorted 1 for random or 2 for reverse sorted list, 
+      sorting type number of runs''')
 string_List = input().split()
-print(string_List)
+print("""Now input the type of sort: 
+      selection,
+      insertion,
+      bubble,
+      merge,
+      quick""")
+sort_type = input()
 
 len_list = int(string_List[0])
 mag_list = int(string_List[1])
@@ -11,20 +22,65 @@ list_type = int(string_List[2])
 num_runs = int(string_List[3])
 
 if list_type == 0:
+    list_of_lists = []
     for i in range(num_runs):
         A = createList.sortedList(len_list, mag_list)
-        insertionSort.insertionSort(A)
+        list_of_lists.append(A)
 
 elif list_type == 1:
+    list_of_lists = []
     for i in range(num_runs):
         A = createList.randomList(len_list, mag_list)
-        insertionSort.insertionSort(A)
+        list_of_lists.append(A)
 
 elif list_type == 2:
+    list_of_lists = []
     for i in range(num_runs):
         A = createList.reverseList(len_list, mag_list)
-        insertionSort.insertionSort(A)
+        list_of_lists.append(A)
 
 else:
     print('please enter valid choice for list type')
 
+if sort_type == "selection":
+    for key in list_of_lists:
+        print(key)
+        start_time = time.time()
+        sortAlgorithms.selectionSort(key)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        print(key)
+
+elif sort_type == "insertion":
+    for key in list_of_lists:
+        print(key)
+        start_time = time.time()
+        sortAlgorithms.insertionSort(key)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        print(key)
+
+elif sort_type == "bubble":
+    for key in list_of_lists:
+        print(key)
+        start_time = time.time()
+        sortAlgorithms.bubbleSort(key)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        print(key)
+
+elif sort_type == "merge":
+    for key in list_of_lists:
+        print(key)
+        start_time = time.time()
+        sortAlgorithms.mergeSort(key)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        print(key)
+
+elif sort_type == "quick":
+    for key in list_of_lists:
+        print(key)
+        start_time = time.time()
+        sortAlgorithms.quickSort(key, 0, len_list - 1)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        print(key)
+
+else:
+    print("Please make sure your spelling is correct")
---
+++ sortAlgorithms.py
@@ -0,0 +1,95 @@
+
+
+def selectionSort(arr):
+
+    # Find the smallest number in the arr
+    for i in range(len(arr) - 1):
+        min_num = i
+
+        for j in range(i+1, len(arr)):
+            if arr[min_num] > arr[j]:
+                min_num = j
+        
+        arr[i], arr[min_num] = arr[min_num], arr[i]
+    
+    return arr
+
+def insertionSort(arr):
+
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and key < arr[j] :
+                arr[j + 1] = arr[j]
+                j -= 1
+        arr[j + 1] = key
+
+    return arr
+
+def bubbleSort(arr):
+    
+    for i in range(len(arr)):
+
+        for j in range(0, len(arr) - i - 1):
+
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    
+    return arr
+
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    leftHalf = arr[:mid]
+    rightHalf = arr[mid:]
+
+    sortedLeft = mergeSort(leftHalf)
+    sortedRight = mergeSort(rightHalf)
+
+    return merge(sortedLeft, sortedRight)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+def partition(array, low, high):
+
+    pivot = array[high]
+
+    i = low - 1
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+
+            i = i + 1
+
+            (array[i], array[j]) = (array[j], array[i])
+
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+    return i + 1
+
+def quickSort(array, low, high):
+    if low < high:
+
+        pi = partition(array, low, high)
+
+        quickSort(array, low, pi - 1)
+
+        quickSort(array, pi + 1, high)
